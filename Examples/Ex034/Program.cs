e88229fc4f2db70e0462f16bd9c53e9998d3f594
Elena <velenavl@inbox.ru>
2022-09-14 21:04:21 +0300

    ex 034

---
+++ Examples/Ex034/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве/
+Console.WriteLine("Введите длину массива:");
+bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+if (!isNumber && length <= 1)
+{
+    Console.WriteLine("Не верно");
+    return;
+}
+
+int[] array = FillArray(length);
+PrintArray(array);
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++) array[i] = random.Next(0, 100);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("Что-то пошло не так");
+        return;
+    }
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine("]");
+}
+
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+int evenNumbers = EvenNumbers(array);
+Console.WriteLine($"Чётных чисел: {evenNumbers}");
