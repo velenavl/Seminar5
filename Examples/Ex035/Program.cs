4468b8dd361f15dbe57fbf3734e0a79b146c5be4
Elena <velenavl@inbox.ru>
2022-09-14 19:57:36 +0300

    ex 035

---
+++ Examples/Ex035/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10, 99].
+Random random = new Random();
+int[] array123 = new int[123];
+
+for (int i = 0; i < array123.Length; i++)
+{
+    array123[i] = random.Next(1, 200);
+}
+printArray(array123);
+System.Console.WriteLine();
+
+System.Console.WriteLine(GetCount(array123));
+
+int GetCount(int[] array)
+{
+    int counter = 0;
+    foreach (var item in array)
+    {
+      if (item >= 10 && item <= 99)
+      {
+        counter++;
+      }  
+    }
+    return counter;
+}
+
+void printArray(int[] array)
+{
+    foreach (int elem in array)
+    {
+        System.Console.Write($"{elem}, ");
+    }
+}
